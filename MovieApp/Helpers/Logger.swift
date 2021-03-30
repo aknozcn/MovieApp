917fb3203ca21ed60318dc7dfb50a3a3a5ef85b3
sh3ll <sh3lll0x0@gmail.com>
2021-03-30 07:17:01 -0700

    logger updated

--- MovieApp/Helpers/Logger.swift
+++ MovieApp/Helpers/Logger.swift
@@ -10,16 +10,8 @@ import Foundation
 class Logger {
 
     static func LogIt(_ msg: String, _ logType: LogType) {
-
-        switch logType {
-        case .succcess:
-            print("\(logType.rawValue) [\(msg)]")
-        case .warning:
-            print("\(logType.rawValue) [\(msg)]")
-        case .error:
-            print("\(logType.rawValue) [\(msg)]")
-        case .action:
-            print("\(logType.rawValue) [\(msg)]")
-        }
+        
+        print("\(logType.rawValue) [\(msg)]")
+        
     }
 }
