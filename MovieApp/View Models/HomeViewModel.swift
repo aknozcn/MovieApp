077ae970f556e2fe525f5b5c974471f82ca11067
Akin <>
2022-10-06 10:11:29 +0300

    duplicate delegate removed

--- MovieApp/View Models/HomeViewModel.swift
+++ MovieApp/View Models/HomeViewModel.swift
@@ -98,7 +98,6 @@ class HomeViewModel {
             case .failure(let error):
                 print("HomeViewModel getMovies \(error)")
             }
-            self.delegate?.didFetchMovies()
         }
     }
 }
